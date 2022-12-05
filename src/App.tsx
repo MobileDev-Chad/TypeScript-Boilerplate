7160249c76206375aea8f44cba744821e232cbe0
WebDev-Chad <chadwin.allotey@gmail.com>
2022-12-04 22:45:56 -0500

    update

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProjectsPage from "./Components/projects/ProjectsPage";
+
+const App = () => {
+  return (
+    <div className="container">
+      <ProjectsPage />
+    </div>
+  );
+};
+
+export default App;
